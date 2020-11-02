e70ce8d2ec019583b3c3c19bd0207e7f61723adb
Ivan Gulyaev <aioki@outlook.com>
2020-11-02 15:06:10 +0400

    Добавлен скрипт удаления БД

---
+++ delete_db.sql
@@ -0,0 +1,16 @@
+USE shedule;
+DROP TABLE istakingplace;
+DROP TABLE subgroup;
+DROP TABLE note;
+DROP TABLE auditorium;
+DROP TABLE couple;
+DROP TABLE subject;
+DROP TABLE student;
+DROP TABLE `group`;
+DROP TABLE teacher;
+DROP TABLE department;
+DROP TABLE faculty;
+DROP TABLE user;
+DROP DATABASE shedule;
+
+
